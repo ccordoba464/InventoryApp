a3c8b2d903487395c153e0a80092206052e58fe3
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2024-09-03 19:09:00 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = { origin: "http://localhost:5173" };
+
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
